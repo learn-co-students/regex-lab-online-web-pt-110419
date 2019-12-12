f0f9e95c5be8f761fa56e4a1ded24106f2fb7269
Mintra Phophaijit <bluembc@gmail.com>
2019-12-12 03:14:32 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,4 +1,9 @@
-def starts_with_a_vowel?(word)
+def starts_with_a_vowelif word.match(/^[AEIOU]|^[aeiou]/)
+    true
+  else
+    false
+  end
+end
 
 end
 
@@ -15,5 +20,11 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
+ if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+    true
+  else
+    false
+  end
+end
 
 end
